f535df1611831923c3bbe77cfdf6c72763b5d64f
Zachary N <taxi.man981@gmail.com>
2024-10-05 11:42:59 +0930

    AI

--- Ludum Dare 56/Assets/AI/Scripts/AIShoot.cs
+++ Ludum Dare 56/Assets/AI/Scripts/AIShoot.cs
@@ -6,21 +6,58 @@ public class AIShoot : AIChase
 {
 	[Header("Shoot")]
 	[SerializeField] GameObject projectileToSummon;
-	[SerializeField] float shootDelay;
+	[SerializeField] Transform spawnPos;
+	
+	[Min(0)][SerializeField] float shootDelay;
 
+	bool CountDown = false;
+	float timeTillNextShoot;
 
 	public override void Activate()
 	{
 		base.Activate();
+		CountDown = true;
+
+		timeTillNextShoot = shootDelay;
 	}
 
 	public override void ActivateUpdate()
 	{
 		base.ActivateUpdate();
+
+		if (CountDown)
+		{
+			timeTillNextShoot -= Time.deltaTime;
+
+			if(timeTillNextShoot <= 0)
+			{
+				Shoot();
+				timeTillNextShoot = shootDelay;
+			}
+		}
 	}
 
 	public override void Deactivate()
 	{
 		base.Deactivate();
+
+		CountDown = false;
+	}
+	public override void Stun()
+	{
+		base.Stun(); 
+		CountDown = false;
+	}
+	public override void StopStun()
+	{
+		base.StopStun();
+		CountDown = true;
+	}
+
+	void Shoot()
+	{
+		var proj = Instantiate(projectileToSummon);
+		proj.transform.position = spawnPos.position;
+		proj.transform.eulerAngles = spawnPos.eulerAngles;
 	}
 }
---
+++ Ludum Dare 56/Assets/AI/Scripts/Projectile.cs
@@ -0,0 +1,63 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Projectile : MonoBehaviour
+{
+	public enum MoveType
+	{
+		MoveForward,
+		HomeOnPlayer,
+		Roll,
+	}
+
+	[SerializeField] MoveType moveType;
+	public MoveType MovementType => moveType;
+	bool Homing => moveType == MoveType.HomeOnPlayer;
+
+	[SerializeField] float lifeTime = -1;
+
+	[SerializeField] float moveSpeed = 5;
+
+	[Header("Other")]
+
+	[ShowIf(nameof(Homing))]
+	[SerializeField] float rotateSpeed = 10;
+
+	float TimeAlive;
+
+	private void Update()
+	{
+		if (lifeTime > 0)
+		{
+			TimeAlive += Time.deltaTime;
+			if (TimeAlive > lifeTime)
+			{
+				DestroyProj();
+			}
+		}
+
+		switch (moveType)
+		{
+			case MoveType.MoveForward:
+				transform.position += transform.forward * moveSpeed * Time.deltaTime;
+				break;
+			case MoveType.HomeOnPlayer:
+				transform.position += transform.forward * moveSpeed * Time.deltaTime;
+				transform.rotation = Quaternion.LookRotation(
+					Vector3.RotateTowards(
+						transform.forward, 
+						PlayerStats.instance.transform.position - transform.position, 
+						rotateSpeed * Time.deltaTime, 
+						0.0f));
+				break;
+		}
+	}
+
+	public void DestroyProj()
+	{
+		Destroy(gameObject);
+	}
+}
