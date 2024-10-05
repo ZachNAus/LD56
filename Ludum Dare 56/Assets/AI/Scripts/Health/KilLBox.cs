85401f4810b786b6c5691ab2b0a272588ad67acf
Zachary N <taxi.man981@gmail.com>
2024-10-05 17:29:06 +0930

    Enemies stop when kill

---
+++ Ludum Dare 56/Assets/AI/Scripts/Health/KilLBox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KilLBox : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.TryGetComponent(out IHasHealth h))
+		{
+			h.TakeDamage(100000);
+		}
+	}
+}
